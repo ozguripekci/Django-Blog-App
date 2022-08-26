642bfc1b36f7b23f1004a81f968d23a50e2798d3
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-08-26 12:03:20 +0200

    models are added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    OPTIONS = {
+        ('draft', 'Draft'),
+        ('public', 'Published'),
+    }
+    title= models.CharField(max_length=50)
+    content = models.TextField(max_length=100)
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    publish_date= models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='draft')
+    slug = models.SlugField(blank=True, unique=True)
+
